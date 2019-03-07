ae7defc0e90fe2d2487a5e33462e1a7abd2e8861
pauty <pautasso.daniele@educ.di.unito.it>
2019-03-07 02:15:48 +0100

    added HazardSpawner advanced logic

--- FlatWars/Assets/Scripts/HazardSpawner.cs
+++ FlatWars/Assets/Scripts/HazardSpawner.cs
@@ -2,42 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HazardSpawner: MonoBehaviour {
     
-    public PrismaBuilder tunnel;
-    public GameObject[] HazardPrefabs = new GameObject[3];
-    public float timeIntervalMin = 1F;
-    public float timeIntervalMax = 2F;
-    float nextTime;
+public class NamedRandomizable{
+    public string name;
+    public float probabilityWeight;
+    
+    public static int GetRandom(NamedRandomizable[] rands){
+        float total = 0;
+        for(int i = 0; i < rands.Length; i++)
+            total += rands[i].probabilityWeight;
+        
+        float r = Random.Range(0F, total-0.0001F);
+        
+        total = rands[0].probabilityWeight;
+        int idx = 0;
+        while(total <= r && idx < rands.Length-1){
+            idx++;
+            total += rands[idx].probabilityWeight;
+        }
+        return idx;   
+    }
+}
+
+[System.Serializable]
+public class HazardFamily : NamedRandomizable{    
+    public GameObject[] prefabs = new GameObject[3];    
+}
+
+[System.Serializable]
+public class SpawnMode : NamedRandomizable{
+    public float[] probabilities = new float[4];
+}
+
+public class HazardSpawner: MonoBehaviour {
+    public PrismaBuilder tunnel; 
+    
+    public HazardFamily[] hazardFamilies = new HazardFamily[3];
+    public SpawnMode[] spawnModes = new SpawnMode[4]; 
+    public float changeModeIntervalMin = 10F;
+    public float changeModeIntervalMax = 10F;
+    public float spawnIntervalMin = 1F;
+    public float spawnIntervalMax = 1F;
+    float nextSpawnTime;
+    float nextChangeTime;
     bool triggerChecked = true;
+    
+    public float[] currentProbabilities;
+    
 	// Use this for initialization
 	void Start () {
-	    nextTime = Time.time + timeIntervalMin;
-	}
-	
+	    nextSpawnTime = Time.time + spawnIntervalMin;
+	    nextChangeTime = Time.time + changeModeIntervalMin;
+	    currentProbabilities = spawnModes[NamedRandomizable.GetRandom(spawnModes)].probabilities;        
+        for(int i = 0; i < hazardFamilies.Length; i++){
+            if(i < currentProbabilities.Length)
+                hazardFamilies[i].probabilityWeight = currentProbabilities[i];
+            else
+                hazardFamilies[i].probabilityWeight = 0F;
+        }
+	}  
+    	
 	// Update is called once per frame
 	void Update () {
-		if(Time.time >= nextTime && triggerChecked){
-		    int i = Random.Range(0, HazardPrefabs.Length);
-		    //i = 3;
-		    GameObject wall = Instantiate(HazardPrefabs[i], transform.position, Quaternion.identity);
+	
+	    if(Time.time >= nextChangeTime){
+	        currentProbabilities = spawnModes[NamedRandomizable.GetRandom(spawnModes)].probabilities;        
+	        for(int i = 0; i < hazardFamilies.Length; i++){
+	            if(i < currentProbabilities.Length)
+	                hazardFamilies[i].probabilityWeight = currentProbabilities[i];
+	            else
+	                hazardFamilies[i].probabilityWeight = 0F;
+	        }
+	        nextChangeTime = Time.time + Random.Range(changeModeIntervalMin, changeModeIntervalMax);
+	    }
+	    
+		if(Time.time >= nextSpawnTime && triggerChecked){
+		    GameObject[] hazardPrefabs = hazardFamilies[NamedRandomizable.GetRandom(hazardFamilies)].prefabs;
+		    
+		    int r = Random.Range(0, hazardPrefabs.Length);
+
+		    GameObject hazard = Instantiate(hazardPrefabs[r], transform.position, Quaternion.identity);
 		    Transform wallScaleX = null;
 		    Transform wallScaleY = null;
 		    Transform wallScaleXY = null;
 		    //
-		    int r = Random.Range(0, 4);
+		    r = Random.Range(0, 4);
 		    switch(r){
 		        case 0:
 		            //DO NOTHING, NO ROTATION
 		            break;
 		        case 1:
-		            wall.transform.Rotate(new Vector3(0,0,180));
+		            hazard.transform.Rotate(new Vector3(0,0,180));
 		            break;
 		        case 3:
-		            wall.transform.Rotate(new Vector3(0,0,90));
+		            hazard.transform.Rotate(new Vector3(0,0,90));
 		            break;
 		        case 4: 
-		            wall.transform.Rotate(new Vector3(0,0,-90));
+		            hazard.transform.Rotate(new Vector3(0,0,-90));
 		            break;
 		    }
 		    float scaleX = tunnel.widthSteps*tunnel.sectorWidth/10;
@@ -49,32 +110,32 @@ public class HazardSpawner: MonoBehaviour {
 		        scaleY = tmp;
 		    }
 
-		    wallScaleX = wall.transform.Find("ScaleX");
+		    wallScaleX = hazard.transform.Find("ScaleX");
 		    if(wallScaleX != null){
 		        wallScaleX.transform.localScale = new Vector3(scaleX, 1, 1);
 		    }
-		    wallScaleY = wall.transform.Find("ScaleY");
+		    wallScaleY = hazard.transform.Find("ScaleY");
 		    if(wallScaleY != null){
 		        wallScaleY.transform.localScale = new Vector3(1, scaleY, 1);
 		    }
-		    wallScaleXY = wall.transform.Find("ScaleXY");
+		    wallScaleXY = hazard.transform.Find("ScaleXY");
 		    if(wallScaleXY != null){
 		        wallScaleXY.transform.localScale = new Vector3(scaleX, scaleY, 1);
 		    }
-		    wall.transform.localScale = new Vector3(1, 1, tunnel.sectorDepth/10);
+		    hazard.transform.localScale = new Vector3(1, 1, tunnel.sectorDepth/10);
 		   
-		    //nextTime = Time.time + timeInterval;  
 		    triggerChecked = false;
 		}
 	}
 	
+	
 	void OnTriggerEnter(Collider coll){
-	    nextTime = Time.time + Random.Range(timeIntervalMin, timeIntervalMax);
+	    nextSpawnTime = Time.time + Random.Range(spawnIntervalMin, spawnIntervalMax);
 	    triggerChecked = true;
 	}
 	
 	void OnTriggerStay(Collider coll){
-	    nextTime = Time.time + Random.Range(timeIntervalMin, timeIntervalMax);
+	    nextSpawnTime = Time.time + Random.Range(spawnIntervalMin, spawnIntervalMax);
 	    triggerChecked = true;
 	}
 	
